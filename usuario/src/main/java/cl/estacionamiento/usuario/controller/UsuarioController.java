6db68ea8d979653f4c9fb97ff9f8745b8f9430a5
Sebastián González P. <se.gonzalez2@duocuc.cl>
2025-04-06 18:12:05 -0400

    Add files via upload

---
+++ usuario/src/main/java/cl/estacionamiento/usuario/controller/UsuarioController.java
@@ -0,0 +1,29 @@
+package cl.estacionamiento.usuario.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cl.estacionamiento.usuario.entity.Usuario;
+import cl.estacionamiento.usuario.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listarUsuarios() {
+        List<Usuario> usuarios = usuarioService.getAll();
+        if (usuarios.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(usuarios);
+    }
+}
