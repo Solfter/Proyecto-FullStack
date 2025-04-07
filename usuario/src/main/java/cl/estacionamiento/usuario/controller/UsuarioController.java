22f5403ae7ceb3cda17b5f22ae986aaeee1681af
Solfter <se.gonzalez2@duocuc.cl>
2025-04-07 18:27:14 -0400

    Primer commit local

---
+++ usuario/src/main/java/cl/estacionamiento/usuario/controller/UsuarioController.java
@@ -0,0 +1,29 @@
+package cl.estacionamiento.usuario.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cl.estacionamiento.usuario.dto.UsuarioDTO;
+import cl.estacionamiento.usuario.service.impl.UsuarioService;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioDTO>> listarUsuarios() {
+        List<UsuarioDTO> usuarios = usuarioService.getAll();
+        if (usuarios.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(usuarios);
+    }
+}
