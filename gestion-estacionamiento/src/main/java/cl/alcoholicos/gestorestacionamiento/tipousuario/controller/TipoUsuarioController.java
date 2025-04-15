c8499698fe4e03f2a8b0c1f59af514d9498c6ac0
Solfter <se.gonzalez2@duocuc.cl>
2025-04-14 20:42:27 -0400

    CRUD tipoUsuario Operativo

--- gestion-estacionamiento/src/main/java/cl/alcoholicos/gestorestacionamiento/tipousuario/controller/TipoUsuarioController.java
+++ gestion-estacionamiento/src/main/java/cl/alcoholicos/gestorestacionamiento/tipousuario/controller/TipoUsuarioController.java
@@ -1,5 +1,66 @@
 package cl.alcoholicos.gestorestacionamiento.tipousuario.controller;
 
-public class TipoUsuarioController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cl.alcoholicos.gestorestacionamiento.tipousuario.dto.TipoUsuarioDTO;
+import cl.alcoholicos.gestorestacionamiento.tipousuario.service.impl.TipoUsuarioService;
+
+@RequestMapping("/tipousuario")
+@RestController
+public class TipoUsuarioController {
+    
+    @Autowired
+    private TipoUsuarioService tipoUsuarioService;
+
+    @GetMapping
+    public ResponseEntity<List<TipoUsuarioDTO>> getAll() {
+        List<TipoUsuarioDTO> tipoUsuarios = tipoUsuarioService.getAll();
+        if (tipoUsuarios.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(tipoUsuarios);  
+    }
+
+    @PostMapping
+    public ResponseEntity<TipoUsuarioDTO> insert(@RequestBody TipoUsuarioDTO tipoUsuario) {
+        TipoUsuarioDTO nuevoTipoUsuario = tipoUsuarioService.insert(tipoUsuario);
+        return ResponseEntity.ok(nuevoTipoUsuario);
+    }
+
+    @PutMapping("/{idTipoUsuario}")
+    public ResponseEntity<TipoUsuarioDTO> update(@PathVariable Integer idTipoUsuario, @RequestBody TipoUsuarioDTO tipoUsuario) {
+        TipoUsuarioDTO tipoUsuarioExistente = tipoUsuarioService.getById(idTipoUsuario);
+        if (tipoUsuarioExistente == null) {
+            return ResponseEntity.notFound().build();
+        }
+        TipoUsuarioDTO tipoUsurioActualizado = tipoUsuarioService.update(idTipoUsuario, tipoUsuario);
+        return ResponseEntity.ok(tipoUsurioActualizado);
+
+    }
+
+    @DeleteMapping("/{idTipoUsuario}")
+    public ResponseEntity<TipoUsuarioDTO> delete (@PathVariable Integer idTipoUsuario) {
+        TipoUsuarioDTO tipoUsuario = tipoUsuarioService.getById(idTipoUsuario);
+        if (tipoUsuario == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        TipoUsuarioDTO tipoUsuarioBorrado = tipoUsuarioService.delete(idTipoUsuario);
+        if (tipoUsuarioBorrado == null) {
+            return ResponseEntity.ok(tipoUsuarioBorrado);
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
 }
--- gestion-estacionamiento/src/main/java/cl/alcoholicos/gestorestacionamiento/tipousuario/service/impl/TipoUsuarioService.java
+++ gestion-estacionamiento/src/main/java/cl/alcoholicos/gestorestacionamiento/tipousuario/service/impl/TipoUsuarioService.java
@@ -3,11 +3,13 @@ package cl.alcoholicos.gestorestacionamiento.tipousuario.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import cl.alcoholicos.gestorestacionamiento.tipousuario.dto.TipoUsuarioDTO;
 import cl.alcoholicos.gestorestacionamiento.tipousuario.repository.TipoUsuarioRepository;
 import cl.alcoholicos.gestorestacionamiento.tipousuario.service.ITipoUsuario;
 
+@Service
 public class TipoUsuarioService implements ITipoUsuario {
 
     @Autowired
