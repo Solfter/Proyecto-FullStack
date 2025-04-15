70aad671b8cab72d0e3a44c23581f4fc020aba0e
Solfter <se.gonzalez2@duocuc.cl>
2025-04-14 20:36:53 -0400

    CRUD Modelo Operativo menos Delete

--- gestion-estacionamiento/src/main/java/cl/alcoholicos/gestorestacionamiento/modelo/controller/ModeloController.java
+++ gestion-estacionamiento/src/main/java/cl/alcoholicos/gestorestacionamiento/modelo/controller/ModeloController.java
@@ -1,5 +1,66 @@
 package cl.alcoholicos.gestorestacionamiento.modelo.controller;
 
-public class ModeloController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cl.alcoholicos.gestorestacionamiento.modelo.dto.ModeloDTO;
+import cl.alcoholicos.gestorestacionamiento.modelo.service.impl.ModeloService;
+
+@RequestMapping("/modelo")
+@RestController
+public class ModeloController {
+    
+    @Autowired
+    private ModeloService modeloService;
+
+    @GetMapping
+    public ResponseEntity<List<ModeloDTO>> getAll() {
+        List<ModeloDTO> modelos = modeloService.getAll();
+        if (modelos.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(modelos);  
+    }
+
+    @PostMapping
+    public ResponseEntity<ModeloDTO> insert(@RequestBody ModeloDTO modelo) {
+        ModeloDTO nuevaMarca = modeloService.insert(modelo);
+        return ResponseEntity.ok(nuevaMarca);
+    }
+
+    @PutMapping("/{idModelo}")
+    public ResponseEntity<ModeloDTO> update(@PathVariable Integer idModelo, @RequestBody ModeloDTO modelo) {
+        ModeloDTO modeloExistente = modeloService.getById(idModelo);
+        if (modeloExistente == null) {
+            return ResponseEntity.notFound().build();
+        }
+        ModeloDTO modeloActualizado = modeloService.update(idModelo, modelo);
+        return ResponseEntity.ok(modeloActualizado);
+
+    }
+
+    @DeleteMapping("/{idModelo}")
+    public ResponseEntity<ModeloDTO> delete (@PathVariable Integer idModelo) {
+        ModeloDTO modelo = modeloService.getById(idModelo);
+        if (modelo == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        ModeloDTO modeloBorrado = modeloService.delete(idModelo);
+        if (modeloBorrado == null) {
+            return ResponseEntity.ok(modeloBorrado);
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
 }
--- gestion-estacionamiento/src/main/java/cl/alcoholicos/gestorestacionamiento/modelo/service/impl/ModeloService.java
+++ gestion-estacionamiento/src/main/java/cl/alcoholicos/gestorestacionamiento/modelo/service/impl/ModeloService.java
@@ -3,11 +3,13 @@ package cl.alcoholicos.gestorestacionamiento.modelo.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import cl.alcoholicos.gestorestacionamiento.modelo.dto.ModeloDTO;
 import cl.alcoholicos.gestorestacionamiento.modelo.repository.ModeloRepository;
 import cl.alcoholicos.gestorestacionamiento.modelo.service.IModelo;
 
+@Service
 public class ModeloService implements IModelo {
 
     @Autowired
