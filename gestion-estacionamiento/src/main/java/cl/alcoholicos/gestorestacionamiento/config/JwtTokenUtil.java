05f4e3f2555b82e8b093b53cab9e6791c515b517
Solfter <se.gonzalez2@duocuc.cl>
2025-06-07 10:30:10 -0400

    front-end de login y reservas implementado

--- gestion-estacionamiento/src/main/java/cl/alcoholicos/gestorestacionamiento/config/JwtTokenUtil.java
+++ gestion-estacionamiento/src/main/java/cl/alcoholicos/gestorestacionamiento/config/JwtTokenUtil.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import cl.alcoholicos.gestorestacionamiento.dto.UsuarioDetails;
 import cl.alcoholicos.gestorestacionamiento.dto.UsuarioResponseDTO;
 
 import javax.crypto.SecretKey;
