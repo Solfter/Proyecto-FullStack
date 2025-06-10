3768f1455811d26d04a20b1a73a4a48c55db2fb5
Solfter <se.gonzalez2@duocuc.cl>
2025-06-09 23:24:55 -0400

    Test: Usuario no Encontrado

--- gestion-estacionamiento/target/generated-sources/annotations/cl/alcoholicos/gestorestacionamiento/mapper/TipoUsuarioMapperImpl.java
+++ gestion-estacionamiento/target/generated-sources/annotations/cl/alcoholicos/gestorestacionamiento/mapper/TipoUsuarioMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-06-09T19:53:18-0400",
+    date = "2025-06-09T23:23:56-0400",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.42.0.v20250514-1000, environment: Java 21.0.7 (Eclipse Adoptium)"
 )
 @Component
