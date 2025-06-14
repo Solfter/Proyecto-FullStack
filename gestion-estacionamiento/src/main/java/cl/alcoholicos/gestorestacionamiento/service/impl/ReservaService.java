cdeac6e9a36601b750d1d779b3ad4f991f1af77a
Solfter <se.gonzalez2@duocuc.cl>
2025-06-14 10:04:04 -0400

    Cambio de estadoEstacionamiento cuando se realiza reserva

--- gestion-estacionamiento/src/main/java/cl/alcoholicos/gestorestacionamiento/service/impl/ReservaService.java
+++ gestion-estacionamiento/src/main/java/cl/alcoholicos/gestorestacionamiento/service/impl/ReservaService.java
@@ -11,10 +11,12 @@ import cl.alcoholicos.gestorestacionamiento.dto.ReservaCreateDTO;
 import cl.alcoholicos.gestorestacionamiento.dto.ReservaResponseDTO;
 import cl.alcoholicos.gestorestacionamiento.dto.ReservaUpdateDTO;
 import cl.alcoholicos.gestorestacionamiento.entity.EstacionamientoEntity;
+import cl.alcoholicos.gestorestacionamiento.entity.EstadoEstacionamientoEntity;
 import cl.alcoholicos.gestorestacionamiento.entity.ReservaEntity;
 import cl.alcoholicos.gestorestacionamiento.entity.UsuarioEntity;
 import cl.alcoholicos.gestorestacionamiento.mapper.ReservaMapper;
 import cl.alcoholicos.gestorestacionamiento.repository.EstacionamientoRepository;
+import cl.alcoholicos.gestorestacionamiento.repository.EstadoEstacionamientoRepository;
 import cl.alcoholicos.gestorestacionamiento.repository.ReservaRepository;
 import cl.alcoholicos.gestorestacionamiento.repository.UsuarioRepository;
 import cl.alcoholicos.gestorestacionamiento.service.IReserva;
@@ -29,6 +31,7 @@ public class ReservaService implements IReserva {
     private final ReservaMapper reservaMapper;
     private final EstacionamientoRepository estacionamientoRepository;
     private final UsuarioRepository usuarioRepository;
+    private final EstadoEstacionamientoRepository estadoEstacionamientoRepository;
 
     @Override
     public ReservaResponseDTO insert(ReservaCreateDTO createDTO, Integer rutUsuario) {
@@ -37,12 +40,17 @@ public class ReservaService implements IReserva {
         EstacionamientoEntity estacionamiento = estacionamientoRepository.findById(createDTO.getIdEstacionamiento())
                                                 .orElseThrow(() -> new EntityNotFoundException("Estacionamiento no encontrado"));
 
+        EstadoEstacionamientoEntity reservado = estadoEstacionamientoRepository.findById(3)
+            .orElseThrow(() -> new EntityNotFoundException("Estado de estacionamiento no encontrado"));
+
         UsuarioEntity usuario = usuarioRepository.findById(rutUsuario)
                                 .orElseThrow(() -> new EntityNotFoundException("Usuario no encontrado"));
 
         if (estacionamiento.getEstadoEstacionamiento().getIdEstadoEstacionamiento() != 1) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "El estacionamiento se encuentra en estado de: " + estacionamiento.getEstadoEstacionamiento().getDescEstadoEstacionamiento());
         }
+
+        estacionamiento.setEstadoEstacionamiento(reservado);
 
         reserva.setEstacionamiento(estacionamiento);
         reserva.setUsuario(usuario);
