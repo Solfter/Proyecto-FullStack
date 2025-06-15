e77f5f4f1964c510bd70d3dad2bd008f1ae9e25a
Solfter <se.gonzalez2@duocuc.cl>
2025-06-15 00:41:57 -0400

    Documentación de la API Gestor de Estacionamiento actualizada en OpenApiConfig

--- gestion-estacionamiento/src/main/java/cl/alcoholicos/gestorestacionamiento/config/OpenApiConfig.java
+++ gestion-estacionamiento/src/main/java/cl/alcoholicos/gestorestacionamiento/config/OpenApiConfig.java
@@ -1,22 +1,25 @@
 package cl.alcoholicos.gestorestacionamiento.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableWebMvc
 public class OpenApiConfig {
-    
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Mi API")
-                        .version("1.0")
-                        .description("Documentación de mi API"));
+            .info(new Info()
+                .title("API Gestor de Estacionamiento")
+                .version("1.0.0")
+                .description("API REST para la gestión de reservas de estacionamiento")
+                .contact(new Contact()
+                    .name("Equipo de Desarrollo")
+                    .email("se.gonzalez2@duocuc.cl")
+                )
+            );
     }
 }
