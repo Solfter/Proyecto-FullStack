1e28e373f9c66c91cf43c96e4eef19b82f726388
Solfter <se.gonzalez2@duocuc.cl>
2025-06-15 00:43:08 -0400

    Eliminada la información de contacto del OpenAPI en OpenApiConfig

--- gestion-estacionamiento/src/main/java/cl/alcoholicos/gestorestacionamiento/config/OpenApiConfig.java
+++ gestion-estacionamiento/src/main/java/cl/alcoholicos/gestorestacionamiento/config/OpenApiConfig.java
@@ -2,7 +2,6 @@ package cl.alcoholicos.gestorestacionamiento.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.Contact;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,10 +15,6 @@ public class OpenApiConfig {
                 .title("API Gestor de Estacionamiento")
                 .version("1.0.0")
                 .description("API REST para la gestión de reservas de estacionamiento")
-                .contact(new Contact()
-                    .name("Equipo de Desarrollo")
-                    .email("se.gonzalez2@duocuc.cl")
-                )
             );
     }
 }
